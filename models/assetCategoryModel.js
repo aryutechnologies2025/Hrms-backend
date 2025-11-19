dd7001915e5e3b73a4168d5e3351731578e941f3
archanaradha95 <archanaradha95@gmail.com>
2025-11-19 17:10:25 +0530

    assets added

--- models/assetCategoryModel.js
+++ models/assetCategoryModel.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 const assetCategoryDetails = new mongoose.Schema({
-    title:{type:String,required:[true,"Please provide a title"]},
-    status:{type:String, required: [true, "Please provide a status"]},
+    name:{
+        type:String,
+        required:[true,"Please provide a name"]
+    },
+    status:{type:String, 
+        required: [true, "Please provide a status"]
+    },
 
    
 } ,{
     timestamps:true,
 });
-assetCategoryDetails.index({ title: 1 }); // 1 = ascending
+assetCategoryDetails.index({ name: 1 }); // 1 = ascending
 
 const AssetCategory = mongoose.model("AssetCategory",assetCategoryDetails);
  export default AssetCategory; 
