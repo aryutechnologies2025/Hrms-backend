b4ef991a30e4ff963378faa164fabf5eef091e05
archanaradha95 <archanaradha95@gmail.com>
2025-11-21 18:37:17 +0530

    statement filter added

--- routes/statementsRouter.js
+++ routes/statementsRouter.js
@@ -1,11 +1,21 @@
 import express from "express";
 import xlUpload from "../middlewares/xlUpload.js";
 
-import importExcel from "../controllers/statementsController.js";
+import  { 
+    deleteStatementDetails, 
+    editStatementDetails, 
+    getAllStatementDetails, 
+    getStatementDetailsById, 
+    importExcel 
+} from "../controllers/statementsController.js";
 
 const statementsRouter = express.Router();
 
 statementsRouter.post("/import", xlUpload.single("file"),importExcel);
+statementsRouter.get("/getAllStatementDetails", getAllStatementDetails);
+statementsRouter.get("/getAllStatementDetailsById/:id", getStatementDetailsById);
+statementsRouter.put("/editStatementDetails/:id", editStatementDetails);
+statementsRouter.delete("/deleteStatementDetails/:id", deleteStatementDetails); 
  
 
 export default statementsRouter;
