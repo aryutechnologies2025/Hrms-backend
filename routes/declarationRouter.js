b53511726181685baa7d1fe161f424c2d3d099fe
BarathKrishnamoorthy <barathkrishnamoorthy17@gmail.com>
2025-12-01 12:28:08 +0530

    updated code

--- routes/declarationRouter.js
+++ routes/declarationRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { createDeclaration, getDeclaration, editDeclaration, deleteDeclaration  } from "../controllers/declarationController.js";
-
+import upload from "../middlewares/upload.js";
 const declarationRouter = express.Router();
 
-declarationRouter.post("/create-declarationlist", createDeclaration);
+declarationRouter.post("/create-declarationlist", upload.any(), createDeclaration);
 declarationRouter.get("/view-declarationlist", getDeclaration);
-declarationRouter.put("/edit-declarationlist/:id", editDeclaration);
+declarationRouter.put("/edit-declarationlist/:id",upload.any(), editDeclaration);
 declarationRouter.delete("/delete-declarationlist/:id", deleteDeclaration);
 
 export default declarationRouter;
