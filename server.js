5ec05059c694dc62f5599bdfdaf66acab5ed7515
Venu-Developer <venu191202@gmail.com>
2025-12-19 17:05:14 +0530

    push

--- server.js
+++ server.js
@@ -182,7 +182,7 @@ const startApp = async () => {
 
 
   // Base route
-  app.get('/api', (req, res) => res.send('API is running...'));
+  app.get('/api', (req, res) => res.send('API is running... hello'));
   const PORT = process.env.PORT || 5000;
   server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 };
